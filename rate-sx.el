04879b059e39b48e7d7ffa9c437ebab14a02b26b
Dave Pearson <davep@davep.org>
2017-12-08 19:45:14 +0000

    Add todo item about supporting different base rates

--- rate-sx.el
+++ rate-sx.el
@@ -22,6 +22,10 @@
 ;;; Commentary:
 ;;
 ;; rate-sx.el provides a command for showing currency rates from rate.sx.
+;;
+;; TODO:
+;;
+;; Support different non-crypto base rates (gbp.rate.sx, etc).
 
 ;;; Code:
 
