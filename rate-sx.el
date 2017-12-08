5dad99e6a9aa4bad74608907afd8f3a1e908d6f8
Dave Pearson <davep@davep.org>
2017-12-08 20:20:35 +0000

    v1.1

--- rate-sx.el
+++ rate-sx.el
@@ -2,7 +2,7 @@
 ;; Copyright 2017 by Dave Pearson <davep@davep.org>
 
 ;; Author: Dave Pearson <davep@davep.org>
-;; Version: 1.00
+;; Version: 1.1
 ;; Keywords: comm, currency, bitcoin, money
 ;; URL: https://github.com/davep/rate-sx.el
 
